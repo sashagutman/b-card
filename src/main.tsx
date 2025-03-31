0c635bcffbc98a23d3e2b9745b16fa191964a3ec
sashagutman <380975287015s@gmail.com>
2025-03-31 22:08:13 +0300

    Add basic project description to README

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/global.css";
@@ -9,11 +9,12 @@ import { SearchProvider } from "./context/SearchContext";
 
 createRoot(document.getElementById("root")!).render(
   <SearchProvider>
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+      <HashRouter> 
+        <App />
+      </HashRouter>
+    </StrictMode>
   </SearchProvider>
 );
+
 
